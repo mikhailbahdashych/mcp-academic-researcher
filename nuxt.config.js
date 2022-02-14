e5353e770be96cb8b5a049b83d3670f9a6ccea59
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-14 22:19:09 +0100

    nuxt config gix

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,6 @@ module.exports = {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     '@nuxtjs/google-fonts'
   ],
 
@@ -57,8 +56,6 @@ module.exports = {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
