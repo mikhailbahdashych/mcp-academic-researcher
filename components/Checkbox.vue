a00aa1ee5af6a8bb5aa601d8328d3e9f318258bf
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-15 23:05:52 +0100

    name

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -6,41 +6,7 @@
 
 <script>
 export default {
-  model: {
-    prop: 'modelValue',
-    event: 'change'
-  },
-  props: {
-    "value": { type: String },
-    "modelValue": { default: "" },
-    "label": { type: String, required: true},
-    "trueValue": { default: true },
-    "falseValue": { default: false }
-  },
-  computed: {
-    isChecked() {
-      if (this.modelValue instanceof Array) {
-        return this.modelValue.includes(this.value)
-      }
-      return this.modelValue === this.trueValue
-    }
-  },
-  methods: {
-    updateInput(event) {
-      let isChecked = event.target.checked
-      if (this.modelValue instanceof Array) {
-        let newValue = [...this.modelValue]
-        if (isChecked) {
-          newValue.push(this.value)
-        } else {
-          newValue.splice(newValue.indexOf(this.value), 1)
-        }
-        this.$emit('change', newValue)
-      } else {
-        this.$emit('change', isChecked ? this.trueValue : this.falseValue)
-      }
-    }
-  }
+  name: "Checkbox"
 }
 </script>
 
