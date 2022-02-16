9e18f5a235a2135605a03603ae74dc129d98f678
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-16 16:43:21 +0100

    comms

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -1,9 +1,11 @@
 <template>
-  <label class="container">Three
-    <input type="checkbox">
-    <span class="checkmark"></span>
-  </label>
-
+  <div style="display: flex; padding-top: 12px; padding-bottom: 12px">
+    <label class="container">
+      <input type="checkbox">
+      <span class="checkmark"></span>
+    </label>
+    <p style="padding-top: 3px; margin: 0">Here is some text</p>
+  </div>
 </template>
 
 <script>
