74a7f6aafd411d93b5b2e4f5b222eb6c476bf2ec
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-17 17:31:41 +0100

    login store

--- store/index.js
+++ store/index.js
@@ -1,11 +1,13 @@
 import Vuex from 'vuex';
 import register from './modules/register'
+import login from './modules/login'
 
 const createStore = () => {
   return new Vuex.Store({
     namespaced: true,
     modules: {
-      register
+      register,
+      login
     }
   })
 }
