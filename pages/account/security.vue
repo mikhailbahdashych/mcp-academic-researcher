4abcb377535f97a3b9b60343555690996d46a4bd
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-21 21:42:58 +0100

    basic components

--- pages/account/security.vue
+++ pages/account/security.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
     <h1>Here is security subpage</h1>
+    <Input />
+    <Button :label="'Set 2FA'" :clickon="set2fa" />
   </div>
 </template>
 
 <script>
 import { set2fa } from "~/api";
+import Input from "~/components/Input";
+import Button from "~/components/Button";
 export default {
-  name: "security"
+  name: "security",
+  components: {
+    Input,
+    Button
+  },
+  methods: {
+    async set2fa() {
+      await set2fa()
+    }
+  }
 }
 </script>
 
