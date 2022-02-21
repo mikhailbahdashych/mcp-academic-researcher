2ed5cbaebe0a17670e72fadd810b598e39709dce
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-21 21:50:34 +0100

    code and token

--- pages/account/security.vue
+++ pages/account/security.vue
@@ -2,7 +2,7 @@
   <div style="padding: 50px; width: 600px; margin: 0 auto">
     <h1>Here is security subpage</h1>
     <Button :label="'Click to generate 2FA'" :clickon="generate2fa" />
-    <Input :title="'Code'" :type="'text'" />
+    <Input :title="'Code'" :type="'text'" v-model="code" />
     <Button :label="'Set 2FA'" :clickon="set2fa" />
   </div>
 </template>
@@ -17,9 +17,18 @@ export default {
     Input,
     Button
   },
+  data() {
+    return {
+      code: null,
+      token: null
+    }
+  },
   methods: {
     async set2fa() {
-      await set2fa()
+      await set2fa({
+        code: this.code,
+        token: this.token
+      })
     },
     generate2fa() {
 
