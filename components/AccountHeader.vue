3421b64fba75128fee6e087e2b1dddc72e7fd4d0
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-24 22:26:31 +0100

    account header

--- components/AccountHeader.vue
+++ components/AccountHeader.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="account-header">
-
+    <div class="account-header-inner">
+      <div v-for="item in accountHeaderItems">
+        <h3>{{item.title}}</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "AccountHeader"
+  name: "AccountHeader",
+  data() {
+    return {
+      accountHeaderItems: [
+        { title: 'My account' },
+        { title: 'Settings' },
+        { title: 'Security' },
+      ]
+    }
+  }
 }
 </script>
 
