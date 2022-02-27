6c4f13437a4202f6d2fec2d20bf624eeef22657d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-27 10:34:19 +0100

    confirm email id

---
+++ pages/confirm-registration/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+
+    <div class="login-content">
+      <h1 @click="redirect('/')">Logo</h1>
+    </div>
+
+    <div class="login-inputs">
+      <div class="login-inputs-container">
+        <h1>Here is some text fomr confirm registr</h1>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "confirm-registration",
+  mounted() {
+    console.log(this.$route.params.id)
+  },
+  methods: {
+    redirect(path) {
+      this.$router.push({ path })
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../assets/css/login";
+</style>
