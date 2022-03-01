23006a1fba3992c043cdff808a098ee323b924b3
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-01 17:28:54 +0100

    loader styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Loader />
     <Waves />
     <div class="home">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta maxime sed temporibus. Cum cumque est ex id ipsam libero, qui quia sunt ut voluptatum? A, aliquam delectus eius est hic id incidunt inventore iusto magni maxime minima necessitatibus nesciunt officiis perferendis, quam quidem, quis quisquam quo rem sapiente vitae voluptatem.
@@ -9,10 +10,12 @@
 
 <script>
 import Waves from "@/components/Waves";
+import Loader from "@/components/Loader";
 export default {
   name: 'IndexPage',
   components: {
-    Waves
+    Waves,
+    Loader
   }
 }
 </script>
