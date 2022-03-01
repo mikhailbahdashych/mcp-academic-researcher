c048d63cddb862025a5caf8081227db8537e06fa
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-01 17:29:29 +0100

    index vue fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Loader />
     <Waves />
     <div class="home">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta maxime sed temporibus. Cum cumque est ex id ipsam libero, qui quia sunt ut voluptatum? A, aliquam delectus eius est hic id incidunt inventore iusto magni maxime minima necessitatibus nesciunt officiis perferendis, quam quidem, quis quisquam quo rem sapiente vitae voluptatem.
@@ -10,12 +9,10 @@
 
 <script>
 import Waves from "@/components/Waves";
-import Loader from "@/components/Loader";
 export default {
   name: 'IndexPage',
   components: {
-    Waves,
-    Loader
+    Waves
   }
 }
 </script>
