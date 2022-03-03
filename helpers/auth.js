872d60260ff08c17a2f6d487b70be45fdb13fd6f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-03 22:42:07 +0100

    auth helper fix

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,15 +1,14 @@
 import { verifyToken } from "@/api";
 
 export const verifyUserToken = async (router) => {
-  if (!localStorage.getItem('token')) {
-    await router.push({path: '/login'})
-  } else {
-    const checkToken = await verifyToken({ token: localStorage.getItem('token') })
-    if (checkToken.error) {
-      localStorage.removeItem('token')
-      await router.push({path: '/login'})
-    }
-  }
+  if (!localStorage.getItem('token')) return await router.push({path: '/login'})
+
+  const checkToken = await verifyToken({ token: localStorage.getItem('token') })
+
+  if (checkToken.status !== -1) return
+
+  localStorage.removeItem('token')
+  await router.push({path: '/login'})
 }
 
 // export const verifyUserTokenSoft = async () => {
