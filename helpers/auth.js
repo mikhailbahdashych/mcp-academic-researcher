4ce3df09e4c6f63ddbb2e07439a06dc641fd3e6c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-03 23:47:21 +0100

    new todo

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,6 +1,7 @@
 import { verifyToken } from "@/api";
 
 export const verifyUserToken = async (router) => {
+  // @TODO DO SOMETHING WITH LOCALSTORAGE
   if (!localStorage.getItem('token')) return await router.push({path: '/login'})
 
   const checkToken = await verifyToken({ token: localStorage.getItem('token') })
