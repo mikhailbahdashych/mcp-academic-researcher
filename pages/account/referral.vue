029ce0255363dc1eacda88c20ba8deb3db0a0ebf
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-07 21:33:22 +0100

    header components

---
+++ pages/account/referral.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <AccountHeader />
+  </div>
+</template>
+
+<script>
+// @TODO CHECK OUT THIS STAFF
+// import AccountHeader from "~/components/AccountHeader";
+export default {
+  name: "referral"
+}
+</script>
+
+<style lang="scss">
+@import "../../assets/css/account";
+</style>
