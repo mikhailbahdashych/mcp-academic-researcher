5e4122b9eea4c3dfc1b3349c291e5526ed105dcc
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-09 21:41:35 +0100

    normal bg

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
-<!--    <Header />-->
-<!--    <div class="main-background" />-->
-    <video autoplay muted controls loop class="test">
+    <Header />
+    <video autoplay muted loop class="main-background">
       <source :src="require('@/assets/video/bg.webm')" type="video/webm">
     </video>
-
-<!--    <div class="main-texts-box">-->
-<!--      <div class="main-welcome">-->
-<!--        <h1 class="title">THE FUTURE IS HERE. THE FUTURE IS NOW.</h1>-->
-<!--        <h1 class="title small-title">Improve your trading strategy with personal AI</h1>-->
-<!--        <h1 class="title small-content">Let your personal Artificial Intelligence collect and analyze trading data to improve your personal strategy</h1>-->
-<!--      </div>-->
-<!--      <div class="main-start-box">-->
-<!--        <h1 class="title small-title">Start now</h1>-->
-<!--        <Button :label="'Start now'" :onhover="true" />-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="main-content"></div>-->
-<!--    <Footer/>-->
+    <div class="main-texts-box">
+      <div class="main-welcome">
+        <h1 class="title">THE FUTURE IS HERE. THE FUTURE IS NOW.</h1>
+        <h1 class="title small-title">Improve your trading strategy with personal AI</h1>
+        <h1 class="title small-content">Let your personal Artificial Intelligence collect and analyze trading data to improve your personal strategy</h1>
+      </div>
+      <div class="main-start-box">
+        <h1 class="title small-title">Start now</h1>
+        <Button :label="'Start now'" :onhover="true" />
+      </div>
+    </div>
+    <div class="main-content"></div>
+    <Footer/>
   </div>
 </template>
 
