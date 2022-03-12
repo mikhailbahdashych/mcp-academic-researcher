05c08e8456ec96a99b9ff6a8003b3c77c511e0e7
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-12 21:54:02 +0100

    footer colums

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,13 @@
 <template>
   <div :class="[bright ? 'footer bright' : 'footer']">
-    <div class="footer-block up"></div>
+    <div class="footer-block up">
+      <div class="footer-item">
+        <p class="paragraph-small medium">About us</p>
+      </div>
+      <div class="footer-item">
+        <p class="paragraph-small medium">Products and services</p>
+      </div>
+    </div>
     <div class="footer-block down">
       <p class="paragraph-small opacity">Citadel 2022</p>
     </div>
