55f79dcffaef1b7cd89f609a5922e55433ecfc5b
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-12 21:40:17 +0100

    footer styles

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
   <div :class="[bright ? 'footer bright' : 'footer']">
+    <div class="footer-block up"></div>
+    <div class="footer-block down">
+      <p class="paragraph-small opacity">Citadel 2022</p>
+    </div>
   </div>
 </template>
 
