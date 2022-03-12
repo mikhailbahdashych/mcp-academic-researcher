87a55912744f757491d0d2a6ee3a66dde91358ec
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-12 16:47:56 +0100

    disalbe 2fa button

--- pages/account/security.vue
+++ pages/account/security.vue
@@ -49,8 +49,8 @@
               description="Are you sure you want to do this?
               If you are, provide code below."
             >
-              <Input :title-class="'on-white-paragraph'" :additional-class="'margin-bottom-20 on-white'" :title="'Provide 6-digit code'" :placeholder="'XXXXXX'" :type="'text'" v-model="securityTwofa.code" :disabled="true" />
-              <Button :label="'Disable 2FA'" :clickon="deactivate2fa" />
+              <Input :title-class="'on-white-paragraph'" :additional-class="'margin-bottom-20 on-white'" :title="'Provide 6-digit code'" :placeholder="'XXXXXX'" :type="'text'" v-model="securityTwofa.code" :disabled="this.$store.getters.getSecurity2fa.status === -3" />
+              <Button :label="'Disable 2FA'" :clickon="deactivate2fa" :disabled="this.$store.getters.getSecurity2fa.status === -3" />
               <div v-if="this.$store.getters.getSecurity2fa.status === -3">
                 <p class="paragraph-small medium on-white-paragraph">Successfully deactivated!</p>
               </div>
