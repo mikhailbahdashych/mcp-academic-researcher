7f79623265cb8691b6d074848c2980ef70061cb5
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-13 10:26:02 +0100

    textarea props

--- components/Textarea.vue
+++ components/Textarea.vue
@@ -1,12 +1,58 @@
 <template>
   <div class="basic-textarea-outer">
-    <textarea class="basic-textarea" />
+    <p :class="`paragraph`">{{ title }}</p>
+    <textarea
+      class="basic-textarea"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      :name="name"
+      :value="innerValue"
+      @input="onInput"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  name: "Textarea"
+  name: "Textarea",
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  watch: {
+    value(value) {
+      this.innerValue = value
+    },
+    innerValue(value) {
+      this.$emit('input', value)
+    }
+  },
+  data() {
+    return {
+      innerValue: this.value
+    }
+  },
+  methods: {
+    onInput(event) {
+      this.$nextTick(() => {
+        this.innerValue = event.target.value
+      })
+    }
+  }
 }
 </script>
 
