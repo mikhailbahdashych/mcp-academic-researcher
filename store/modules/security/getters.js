f0d44dbc8649065d79f456c8b8669bcc61257705
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-13 12:59:33 +0100

    store iteration change

--- store/modules/security/getters.js
+++ store/modules/security/getters.js
@@ -2,5 +2,6 @@ export default {
   getSecurity2fa: state => state.securityTwofa,
   getSecurityPassword: state => state.securityPassword,
   getSecurityEmail: state => state.securityEmail,
-  getSecurityShowModal: state => state.securityShowModal
+  getSecurityShowModal: state => state.securityShowModal,
+  getSecurityOptions: state => state.securityOptions
 }
--- store/modules/security/index.js
+++ store/modules/security/index.js
@@ -26,6 +26,14 @@ const state = () => ({
     changePassword: false,
     disable2fa: false
   },
+  securityOptions: [
+    { title: 'Login password', text: 'Used for account login', buttonTitle: 'Change password', showModalParam: 'changePassword' },
+    { title: 'Login email', text: 'Used for account login. Be careful, you are able to change email only one time.', buttonTitle: 'Change email', showModalParam: 'changeEmail' },
+    { title: 'Google Authenticator', text: 'Used from the verification in account actions, etc. Could be switched to phone verification.', buttonTitle: 'Set 2FA', showModalParam: 'ga' },
+    { title: 'Phone', text: 'Used from the verification in account actions, etc. Could be switched to Google Authenticator verification.', buttonTitle: 'Confirm phone', showModalParam: 'sms' },
+    { title: 'Freeze account', text: 'When this feature is enabled, your account\'s activity will be stopped. You may apply to unfreeze your account the next time you log in.', buttonTitle: 'Freeze account', showModalParam: '' },
+    { title: 'Close account', text: 'All account data will be removed without opportunity of recovering!', buttonTitle: 'Freeze account', showModalParam: 'closingAccount' },
+  ]
 })
 
 export default {
