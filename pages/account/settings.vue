163d190bb0db787cba180f07cb94f18af5287fb0
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-15 22:53:53 +0100

    some setting staff page

--- pages/account/settings.vue
+++ pages/account/settings.vue
@@ -3,7 +3,22 @@
     <Header />
     <AccountHeader />
     <div class="account-container">
+      <div class="account-container-item" v-for="item in settingsOptions">
+        <div class="account-container-item-icon"></div>
+        <div class="account-container-item-icon-texts">
+          <div class="account-container-item-icon-texts text">
+            <p class="paragraph large bold">{{ item.title }}</p>
+          </div>
+          <div class="account-container-item-icon-texts text">
+            <p class="paragraph opacity">{{ item.text }}</p>
+          </div>
+        </div>
 
+        <div class="account-container-item-button">
+          <Button :label="``" />
+        </div>
+
+      </div>
     </div>
     <Footer :bright="true" />
   </div>
@@ -13,12 +28,21 @@
 import { getUserByToken } from "~/helpers/auth";
 export default {
   name: "settings",
+  data() {
+    return {
+      settingsOptions: [
+        { title: 'Language', text: 'Interface language' },
+        { title: 'Theme', text: 'Chose dark or light theme' },
+        { title: 'Default currency', text: 'Chose default currency for pages, etc.' }
+      ]
+    }
+  },
   async mounted() {
     await getUserByToken(this.$router, localStorage.getItem('token'))
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+@import "../../assets/css/account";
 </style>
