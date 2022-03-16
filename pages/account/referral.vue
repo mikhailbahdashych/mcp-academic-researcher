232fe89782f9b18dc0b632ecdcd30c4df760117b
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-16 23:19:21 +0100

    reflink controller

--- pages/account/referral.vue
+++ pages/account/referral.vue
@@ -5,15 +5,21 @@
     <div class="account-container center">
       <h1>Generate your referral link, invite friends and get bonuses</h1>
       <p>By generating your own referral link you are able to send it to your friends and make bonuses. C'mon, it's much fun together!</p>
-      <Button class="center-button" :label="'Generate referral link'" />
+      <Button class="center-button" :label="'Generate referral link'" :clickon="generateRefLink" />
     </div>
     <Footer :bright="true" />
   </div>
 </template>
 
 <script>
+import { generateReferralLink } from "~/api";
 export default {
-  name: "referral"
+  name: "referral",
+  methods: {
+    async generateRefLink() {
+      const res = await generateReferralLink({ token: localStorage.getItem('token') })
+    }
+  }
 }
 </script>
 
