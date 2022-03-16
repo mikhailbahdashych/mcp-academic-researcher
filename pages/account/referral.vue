d6e852f5c83ed3ddc3a945a58bbd26b976f2b785
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-16 22:02:56 +0100

    basic front

--- pages/account/referral.vue
+++ pages/account/referral.vue
@@ -2,7 +2,11 @@
   <div>
     <Header />
     <AccountHeader />
-    <div class="account-container"></div>
+    <div class="account-container center">
+      <h1>Generate your referral link, invite friends and get bonuses</h1>
+      <p>By generating your own referral link you are able to send it to your friends and make bonuses. C'mon, it's much fun together!</p>
+      <Button class="center-button" :label="'Generate referral link'" />
+    </div>
     <Footer :bright="true" />
   </div>
 </template>
