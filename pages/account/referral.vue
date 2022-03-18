19997b20c7825e486accab85ae6bb772a131244d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-18 20:12:19 +0100

    click on to copy

--- pages/account/referral.vue
+++ pages/account/referral.vue
@@ -8,11 +8,13 @@
       <Button class="center-button" :label="'Generate referral link'" :clickon="generateRefLink" />
     </div>
     <div class="account-container" v-else>
-      <div style="display: flex;">
-        <input id="reflink" :value="`http://localhost:8010/register/${reflink}`" type="hidden" />
-        <h3>Here is your referral link: http://localhost:8010/register/{{ reflink }}</h3>
-        <Button :label="'Copy link'" :clickon="copyLink" />
-      </div>
+
+      <input id="reflink" :value="`http://localhost:8010/register/${reflink}`" type="hidden" />
+      <p class="paragraph medium">
+        Here is your referral link (click on to copy):
+        <span class="paragraph link average pointer" @click="copyLink">http://localhost:8010/register/{{ reflink }}</span>
+      </p>
+
       <p class="paragraph medium" v-if="reflinkclients.length === 0">There is no clients registered by your referral link :(</p>
       <p class="paragraph medium">List of clients who has been registered from your link:</p>
     </div>
