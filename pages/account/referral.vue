ba3d0a75b5ef2e532c4c57fea5b7a7f53e170442
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-18 18:27:52 +0100

    reflink fixes

--- pages/account/referral.vue
+++ pages/account/referral.vue
@@ -2,13 +2,14 @@
   <div>
     <Header />
     <AccountHeader />
-    <div class="account-container center" v-if="!reflink">
+    <div class="account-container center" v-if="reflink.status === -1">
       <h1>Generate your referral link, invite friends and get bonuses</h1>
       <p>By generating your own referral link you are able to send it to your friends and make bonuses. C'mon, it's much fun together!</p>
       <Button class="center-button" :label="'Generate referral link'" :clickon="generateRefLink" />
     </div>
     <div class="account-container" v-else>
       <h3>Here is your referral link: {{ reflink }}</h3>
+      <p class="paragraph medium" v-if="reflinkclients.length === 0">There is no clients registered by your referral link :(</p>
       <p class="paragraph medium">List of clients who has been registered from your link:</p>
     </div>
     <Footer :bright="true" />
@@ -21,12 +22,13 @@ export default {
   name: "referral",
   data() {
     return {
-      reflink: null
+      reflink: {},
+      reflinkclients: []
     }
   },
   async mounted() {
     this.reflink = await getReferralLink({ token: localStorage.getItem('token') })
-    await getClientsByReferralLink(this.reflink)
+    this.reflinkclients = await getClientsByReferralLink(this.reflink)
   },
   methods: {
     async generateRefLink() {
