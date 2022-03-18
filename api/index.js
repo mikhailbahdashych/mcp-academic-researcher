fe45b43a9a2a679d2f60def50246348b7a967e9f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-18 16:56:21 +0100

    get back to auth middleware

--- api/index.js
+++ api/index.js
@@ -10,6 +10,15 @@ const api = axios.create({
   }
 })
 
+api.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token')
+  if (token) config.headers.common['Authorization'] = 'Bearer ' + token
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+
 export const login = async (payload) => {
   try {
     const { data } = await api.post(`/l`, payload)
