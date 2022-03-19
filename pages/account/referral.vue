b4dfdcb01527854b436ef01068778f3bcebb2b59
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-19 14:42:08 +0100

    hide email on backend

--- pages/account/referral.vue
+++ pages/account/referral.vue
@@ -13,7 +13,7 @@
       <input id="reflink" :value="`http://localhost:8010/register/?reflink=${reflink}`" type="hidden" />
       <p class="paragraph medium">
         Here is your referral link (click on to copy):
-        <span class="paragraph link average pointer" @click="copyLink">http://localhost:8010/register/?reflink={{ reflink }}</span>
+        <span class="paragraph link average pointer" @click="copyLink">localhost:8010/register/?reflink={{ reflink }}</span>
       </p>
 
       <p class="paragraph medium" v-if="reflinkclients.length === 0">There is no clients registered by your referral link :(</p>
