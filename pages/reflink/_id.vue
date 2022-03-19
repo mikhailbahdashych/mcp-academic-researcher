cd8e51fd0b77c049f23b2d9ee6cab61fe894fbb5
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-19 22:23:58 +0100

    width fixes

--- pages/reflink/_id.vue
+++ pages/reflink/_id.vue
@@ -30,20 +30,19 @@
         <p v-if="passwordError.passwordMismatch" class="paragraph error">Passwords have to match!</p>
         <p v-if="passwordError.passwordRequirement" class="paragraph error">Password are requirement!</p>
 
-<!--        <div v-if="passwordError.passwordRules" class="password-requirement">-->
-
-<!--          <div v-for="rule in passwordRulesList" class="flex">-->
-<!--            <div v-for="(item) in Object.entries(rule)">-->
-<!--              <p>-->
-<!--                <span v-if="item[0] === 'text'">{{ item[1] }}</span>-->
-<!--                <span v-else>-->
-<!--                  <span class="paragraph medium success" v-if="item[1]">OK</span>-->
-<!--                  <span class="paragraph medium error" v-else>NOT OK</span>-->
-<!--                </span>-->
-<!--              </p>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
+        <div v-if="passwordError.passwordRules">
+          <div v-for="rule in passwordRulesList" class="flex">
+            <div v-for="(item) in Object.entries(rule)">
+              <p>
+                <span v-if="item[0] === 'text'">{{ item[1] }}</span>
+                <span v-else>
+                  <span class="paragraph medium success" v-if="item[1]">OK</span>
+                  <span class="paragraph medium error" v-else>NOT OK</span>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
 
         <Checkbox v-model="tac" :label="`I have read and accepted <a href='/'>terms and conditions.</a>`" :disabled="reflink.status === -1" />
         <Button :clickon="register" :label="'Create account'" :disabled="reflink.status === -1 || !validFields()" />
