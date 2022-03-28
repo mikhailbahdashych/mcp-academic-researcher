83bf5189ed50e540728cf953791b23ba33c7640e
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-28 21:11:08 +0200

    working store

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -6,8 +6,8 @@
       <div class="account-container-wrap">
         <h1>Account Information</h1>
         <div class="account-container-info">
-          <Panel :title="'UUID'" :text="`Your UUID: ${client.personaluuid}`" />
-          <Panel :title="'Email'" :text="`Your email: ${client.email}`" :img="'email'" />
+          <Panel :title="'UUID'" :text="`Your UUID: ${this.$store.getters.getClientData.personaluuid}`" />
+          <Panel :title="'Email'" :text="`Your email: ${this.$store.getters.getClientData.email}`" :img="'email'" />
           <Panel :title="'Phone'" :text="'Your mobile phone'" :img="'phone'" />
           <Panel :title="'2FA'" :text="'Your Google Authenticator status'" :img="'google'" />
         </div>
@@ -26,13 +26,9 @@ import { verifyClientByToken } from "~/helpers/auth";
 import { checkWallets } from "~/api/wallet";
 export default {
   name: "account",
-  data() {
-    return {
-      client: {}
-    }
-  },
   async mounted() {
-    this.client = await verifyClientByToken(this.$router, localStorage.getItem('token'))
+    const client = await verifyClientByToken(this.$router, localStorage.getItem('token'))
+    this.$store.commit('setClientData', client)
     // this.client.wallets = await checkWallets({ token: localStorage.getItem('token') })
   }
 }
