daba94360769b2b14f2a29e2efa77dff435d142a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2026-03-06 12:29:43 +0500

    dockerization of the application

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:4200' });
+  app.enableCors({ origin: process.env.CORS_ORIGIN ?? 'http://localhost:4200' });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
